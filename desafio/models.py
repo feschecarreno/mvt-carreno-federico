1eaf89bbb47eb6bf3c5d5e9683c3bc3364ae1f85
Federico Carreño <f.n.carreno@gmail.com>
2022-10-27 16:05:22 -0300

    solucione problema de familiar.objects en models.py

--- desafio/models.py
+++ desafio/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-class Familiar(models.Model):
-      nombre = models.CharField(max_length=100)
-      direccion = models.CharField(max_length=200)
-      numero_dni = models.IntegerField()
-      fecha_de_nacimiento = models.DateField(auto_now_add=False, auto_now=False, null=True)
 
-def __str__(self):
-      return f"Nombre:{self.nombre}, direccion: {self.direccion},Dni: {self.numero_dni},Fecha_de_nacimiento:{self.fecha_de_nacimiento},{self.id}" 
+class Familiar(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    numero_dni = models.IntegerField()
+    fecha_de_nacimiento = models.DateField(auto_now_add=False, auto_now=False, null=True)
+
+    def __str__(self):
+      return f"{self.nombre}, {self.numero_dni}, {self.fecha_de_nacimiento}, {self.id}"
 
 
